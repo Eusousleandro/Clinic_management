1e6382a8c7673525c9561dbe0f9be69189622707
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-25 12:45:15 -0300

    Implementation of interface od service.

---
+++ app/domain/repository/service_repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from app.domain.entities.service import ServiceEntity
+
+
+class IServiceRepository(ABC):
+
+    @abstractmethod
+    def get_All(self) -> List[ServiceEntity]:
+        pass
+
+    @abstractmethod
+    def get_user_id(self, id: int) -> Optional[ServiceEntity]:
+        pass
+
+    @abstractmethod
+    def create(self, service: ServiceEntity) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, service: ServiceEntity) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, id: int) -> None:
+        pass
