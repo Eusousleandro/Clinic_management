d8e08ecd4af83c5cce1d5fbce3751543cb2742d7
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-05-09 18:21:53 -0300

    Refactoring the code, and refactoring o dockerfile.

--- app/infrastructure/repositories/service_repository_implementation.py
+++ app/infrastructure/repositories/service_repository_implementation.py
@@ -1,34 +1,30 @@
 from sqlalchemy.orm import Session
-
 from app.domain.repository.service_repository import IServiceRepository
 from app.infrastructure.database.models.service_model import Service
-from app.interface.schemas.service_schema import ServiceUpdate
-
+from app.interface.schemas.service_schema import ServiceUpdate, ServiceCreate
 
 class ServiceRepository(IServiceRepository):
     def __init__(self, db: Session):
         self.db = db
 
-    async def get_services(self):
-        return self.db.query(Service).all
-    
-    async def get_service_id(self, id: int):
+    async def get_All(self):
+        return self.db.query(Service).all()
+
+    async def get_user_id(self, id: int):
         return self.db.query(Service).filter(Service.id == id).first()
 
-    async def create(self, service_data: Service):
+    async def create(self, service_data: ServiceCreate):
         new_service = Service(**service_data.dict())
         self.db.add(new_service)
         self.db.commit()
         self.db.refresh(new_service)
         return new_service
-    
-    async def update(self, service: ServiceUpdate):
-        service = self.db.query(Service).filter(Service.id == service.id).first()
-        service_data = service.model_dump(exclude_unset=True)
 
-        for key, value in service_data.items():
+    async def update(self, id: int, service_data: ServiceUpdate):
+        service = self.db.query(Service).filter(Service.id == id).first()
+        update_data = service_data.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
             setattr(service, key, value)
-            
         self.db.commit()
         self.db.refresh(service)
         return service
--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
+from app.infrastructure.database.connection import Base, engine
 from app.interface.handler.exception_service import Service_exception_handler
 from app.interface.api.service_controller import router as service_router
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 Service_exception_handler(app)
 app.include_router(service_router)
