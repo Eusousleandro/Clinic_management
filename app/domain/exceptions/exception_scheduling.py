664490839c416205d32121a8adc9a18634505c83
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-05-11 19:24:28 -0300

    Implementation of exception of scheduling.

---
+++ app/domain/exceptions/exception_scheduling.py
@@ -0,0 +1,17 @@
+class NotFoundScheduling(Exception):
+    pass
+
+class FindScheduling(Exception):
+    pass
+
+class AlreadyExistsScheduling(Exception):
+    pass
+
+class CreateScheduling(Exception):
+    pass
+
+class UpdateScheduling(Exception):
+    pass
+
+class DeleteScheduling(Exception):
+    pass
