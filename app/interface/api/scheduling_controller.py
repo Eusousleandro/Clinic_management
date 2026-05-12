1df9b313e1cbd3af37c720e5b8268bfa1e097818
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-05-12 16:12:08 -0300

    Implementation of scheduling controller.

---
+++ app/interface/api/scheduling_controller.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+
+from app.application.use_cases.scheduling import Scheduling
+from app.interface.schemas.scheduling_schemas import SchedulingCreate, SchedulingResponse, SchedulingUpdate
+
+router = APIRouter(prefix='/scheduling', tags=['schedulings'])
+
+@router.get('/', response_model=List[SchedulingResponse])
+async def get_All(
+    scheduling: Scheduling = Depends()
+):
+    return await scheduling.get_scheduling()
+
+@router.get('/{id}', response_model=SchedulingResponse)
+async def get_all_scheduling(
+    id: int,
+    scheduling: Scheduling = Depends(id)
+):
+    return await scheduling.get_scheduling_id(id)
+
+@router.post('/', response_model=SchedulingResponse)
+async def scheduling_create(
+    schedul: SchedulingCreate,
+    scheduling: Scheduling = Depends()
+):
+    return await scheduling.create_scheduling(schedul)
+
+@router.put('/{id}', response_model=SchedulingResponse)
+async def scheduling_update(
+    id: int,
+    schedul: SchedulingUpdate,
+    scheduling: Scheduling = Depends()
+):
+    return await scheduling.update_scheduling(id, schedul)
+
+@router.delete('/{id}', response_model=SchedulingResponse)
+async def scheduling_delete(
+    id: int,
+    scheduling: Scheduling = Depends()
+):
+    return await scheduling.delete_scheduling(id)
