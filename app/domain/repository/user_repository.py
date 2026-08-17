db07f6ff13609477f03d616b2b21e80420363166
unknown <Eusousleandro>
2026-08-17 14:52:48 -0300

    Implementação dos exception do user, e o repositório do user.

--- app/domain/repository/user_repository.py
+++ app/domain/repository/user_repository.py
@@ -5,18 +5,22 @@ from app.domain.entities.user import UserEntity
 
 def IUserRepository():
 
+    @abstractmethod
     def get_All(self) -> List[UserEntity]:
         pass
 
+    @abstractmethod
     def get_user_id(self, id: int) -> Optional[UserEntity]:
         pass
 
+    @abstractmethod
     def create(self, user: UserEntity) -> None:
         pass
 
+    @abstractmethod
     def update(self, user: UserEntity) -> None:
         pass
 
-
+    @abstractmethod
     def delete(self, id: int) -> None:
         pass    
