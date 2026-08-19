835371b4d17f30a09ad716bc533888b857ff9452
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-08-19 19:35:43 -0300

    Implementando a rota dos agendamentos.

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.infrastructure.database.connection import Base, engine
 from app.interface.handler.exception_service import Service_exception_handler
 from app.interface.api.service_controller import router as service_router
+from app.interface.api.scheduling_controller import router as scheduling_router
 
 app = FastAPI()
 
@@ -10,3 +11,4 @@ Base.metadata.create_all(bind=engine)
 
 Service_exception_handler(app)
 app.include_router(service_router)
+app.include_router(scheduling_router)
