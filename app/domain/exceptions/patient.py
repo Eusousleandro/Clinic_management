fbab132e7e4d53c98f68d107eae6e06fca78cadd
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-08-20 17:09:22 -0300

    Implementando as exceções de errros dos pacientes.

---
+++ app/domain/exceptions/patient.py
@@ -0,0 +1,17 @@
+class NotFoundPatient(Exception):
+    pass
+
+class FindPatient(Exception):
+    pass
+
+class AlreadyExistsPatient(Exception):
+    pass
+
+class NotSucessCreatePatient(Exception):
+    pass
+
+class NotSucessUpdatePatient(Exception):
+    pass
+
+class NotSucessDeletePatient(Exception):
+    pass
