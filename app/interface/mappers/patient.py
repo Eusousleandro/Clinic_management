cc6dbf91be6e3ff3a995dfb95a03a572faac15b5
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-08-21 02:27:40 -0300

    Configuração do mapper

--- app/interface/mappers/patient.py
+++ app/interface/mappers/patient.py
@@ -2,5 +2,16 @@ from app.interface.schemas.patient import PatientResponse
 
 def to_patient_response() -> PatientResponse:
     return PatientResponse(
-        
+        id=,
+        name=,
+        email=,
+        cpf=,
+        date_of_birth=,
+        sex=,
+        phone=,
+        address=,
+        city=,
+        state=,
+        code=,
+        observations=
     )
